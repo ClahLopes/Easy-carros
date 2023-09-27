d04aecd999978e74b58440b6bfd93c5a8790aa47
Crdzcode <78108996+Crdzcode@users.noreply.github.com>
2023-09-26 23:36:36 -0300

    feat: initial structure

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# EasyCarros
+
+Projeto criado para o Challenge BRQ apresentado pela FIAP.
+
+### Como rodar o projeto 🧑‍💻
+
+Existem duas formas para rodar o projeto.
+
+- Usando a extensão [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) do VS Code.
+- Usando o Live Server do NPM
+
+#### Live Server do NPM
+
+Para utilizar o Live Server do NPM é simples, basta seguir os passos abaixo:
+
+```bash
+  npm install -g live-server
+  live-server
+```
+
+#### Live Server do VS Code
+
+Basta instalar a extensão e clicar no botão da imagem.
+
+![Imagem](https://github.com/ritwickdey/vscode-live-server/raw/HEAD/images/Screenshot/vscode-live-server-statusbar-3.jpg)
---
+++ app/components/header/header.component.js
@@ -0,0 +1,27 @@
+// Como estamos usando componentes sem estar usando um framework, o template tem que ser definido via js
+const template = document.createElement('template')
+template.innerHTML = `
+    <header>
+        <section class="container">
+            <img class="logo" src="../../../assets/logo/logo.svg" alt="Logo EasyCarros">
+        </section>
+    </header>
+    <link rel="stylesheet" href="./components/header/header.component.css">
+`
+
+class HeaderComponent extends HTMLElement {
+    // O Construtor define propriedades que serão instanciadas junto com o componente, e que podem ser acessada depois caso sejam disponibilizadas para o componente pai
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({mode: 'closed'});
+        // Clonar o template e adicionar ele ao DOM usando o shadowRoot
+        shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    // Essa função é rodada sempre que o componente é carregado pelo componente pai
+    connectedCallback() {
+    }
+}
+
+customElements.define( 'easy-header', HeaderComponent );
