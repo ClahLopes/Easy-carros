483babae780185939f01131a961bd98490db881e
Crdzcode <78108996+Crdzcode@users.noreply.github.com>
2023-09-30 20:45:56 -0300

    feat: new screen: summary

--- app/components/button/button.component.js
+++ app/components/button/button.component.js
@@ -13,8 +13,6 @@ class ButtonComponent extends HTMLElement {
                 <button type="button" class="btn btn-success"></button>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
                 <style>
-                    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-
                     .button_container{
                         position: fixed;
                         bottom: 16px;
